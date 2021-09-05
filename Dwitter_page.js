34fa81ccfd036bb5cca9791b58ba2341f14717ef
Ekansh-Coder5 <78861687+Ekansh-Coder5@users.noreply.github.com>
2021-09-05 13:03:45 +0530

    Add files via upload

--- Dwitter_page.js
+++ Dwitter_page.js
@@ -1,31 +1,65 @@
 //YOUR FIREBASE LINKS
-
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
-         firebase_message_id = childKey;
-         message_data = childData;
-//Start code
-var firebaseConfig = {
+const firebaseConfig = {
       apiKey: "AIzaSyAn0ieWBRRoeXM_sQq3ZwKYj4_8W2Ocapk",
       authDomain: "dropject-b1c62.firebaseapp.com",
       projectId: "dropject-b1c62",
       storageBucket: "dropject-b1c62.appspot.com",
       messagingSenderId: "835668857529",
-      appId: "1:835668857529:web:a07d0b332b970101364168",
-      measurementId: "G-63EL40KPDX"
+      appId: "1:835668857529:web:b05c5c7984218fd8364168",
+      measurementId: "G-JSQLDETXVG"
     };
+    
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    const app = initializeApp(firebaseConfig);
     U_name = localStorage.getItem("U_name");
     R_name = localStorage.getItem("R_name");
 
     function send() {
-          msg = document.getElementById('msg').value;
-          firebase.database().ref(R_name).push({
-                name : U_name,
-                msg : msg,
-                like : 0
-          });
+      msg = document.getElementById('msg').value;
+      firebase.database().ref(R_name).push({
+            name : U_name,
+            msg : msg,
+            like : 0
+            
+      });
+      
           document.getElementById('msg').value ="";
 
 
       }
+
+function getData() { firebase.database().ref("/"+R_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+
+//Start code
+console.log(firebase_message_id);
+console.log(message_data);
+
+name = message_data["name"];
+message = message_data["msg"];
+like = message_data["like"];
+
+name_tag = "<h4>" + name + "<img class='user_tick'> <src='tick.png'> </h4>";
+message_tag = "<h4 class='message_h4'>" + message + "</h4>";
+
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+ " value="+like+ " onclick='updateLike(this.id)'>";
+span_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+row = name_tag + message_tag +like_button + span_tag;
+document.getElementById("output").innerHTML += row;
+
+
+//End code
+}});}); }
+
+getData();
+
+function updateLike(message_id) {
+      console.log("likedmsgid - " + message_id);
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
+      updatedLikes = Number(likes) + 1;
+      firebase.database().ref(R_name).child(message_id).update ({
+            like : updatedLikes
+      })
+}
--- Dwitter_room.js
+++ Dwitter_room.js
@@ -1,44 +1,55 @@
-var firebaseConfig = {
-      apiKey: "AIzaSyAn0ieWBRRoeXM_sQq3ZwKYj4_8W2Ocapk",
-      authDomain: "dropject-b1c62.firebaseapp.com",
-      projectId: "dropject-b1c62",
-      storageBucket: "dropject-b1c62.appspot.com",
-      messagingSenderId: "835668857529",
-      appId: "1:835668857529:web:a07d0b332b970101364168",
-      measurementId: "G-63EL40KPDX"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    U_name = localStorage.getItem("U_name");
+const firebaseConfig = {
+       apiKey: "AIzaSyAn0ieWBRRoeXM_sQq3ZwKYj4_8W2Ocapk",
+       authDomain: "dropject-b1c62.firebaseapp.com",
+       projectId: "dropject-b1c62",
+       storageBucket: "dropject-b1c62.appspot.com",
+       messagingSenderId: "835668857529",
+       appId: "1:835668857529:web:b05c5c7984218fd8364168",
+       measurementId: "G-JSQLDETXVG"
+     };
+     
+     // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+U_name = localStorage.getItem("U_name");
 document.getElementById('U_name').innerHTML = "Welcome " + U_name +"!"
-
+ 
+ 
 function add_room() {
-
-    R_name = document.getElementById('R_name').value;
-    firebase.database().ref("/").child(R_name).update({
-           purpose : "adding room name" 
-    });
-    
-    localStorage.setItem("R_name", R_name);
-    window.location = "Dwitter_page.html"
-    
-    
-    localStorage.setItem("R_name", R_name);
-    window.location = "Dwitter_page.html"
-    
-    }
-    
-    
-    
-    
-    
-    function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-           Room_names = childKey;
-          //Start code
-          row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
-    
-          document.getElementById('output').innerHTML += row;
-          //End code
-          });});}
-    getData();
-        
+ 
+R_name = document.getElementById('R_name').value;
+firebase.database().ref("/").child(R_name).update({
+        purpose : "adding room name" 
+});
+ 
+localStorage.setItem("R_name", R_name);
+window.location = "Dwitter_page.html"
+ 
+}
+ 
+ 
+ 
+ 
+ 
+function getData() {
+       firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
+       snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+       //Start code
+       row = "<div class='room_name' id="+Room_names+" onclick='Redirect(this.id)' >#"+ Room_names +"</div><hr>";
+ 
+       document.getElementById('output').innerHTML += row;
+       //End code
+       });});}
+       
+getData();
+ 
+function Redirect(name) {
+       localStorage.setItem('Roome_name', name);
+       window.location = "Dwitter_page.html";
+}
+ 
+function logout() {
+       localStorage.removeItem('U_name');
+       localStorage.removeItem('R_name');
+       window.location = "index.html"
+}
